6fd4e035b79b386d1227ce83fc4bf908d347a67b
Joshua Espinoza <josh6669@gmail.com>
2022-03-10 23:00:53 -0300

    Cuenta Regresiva vue

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,39 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <div class="row">
+      <h1>Cuenta Regresiva</h1>
+      <h2>{{ countdown }} segundos</h2>
+      <div class="">
+        <button class="btn btn-primary mx-2" @click="iniciar(3)">3 seg</button>
+        <button class="btn btn-danger mx-2" @click="iniciar(10)">10 seg</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      countdown: 0,
+      myInterval: "",
+    };
+  },
+  methods: {
+    iniciar(time) {
+      this.countdown = time;
+      clearInterval(this.myInterval);
+      this.myInterval = setInterval(() => {
+        if (this.countdown > 0) this.countdown--;
+        else clearInterval(this.myInterval);
+      }, 1000);
+    },
+  },
+};
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +41,13 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  ul {
+    li {
+      color: blue;
+    }
+  }
+}
+h2 {
+  color: gray;
 }
 </style>
