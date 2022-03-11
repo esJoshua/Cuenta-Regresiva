289b19d120b3845fadcabbd403b6b397b2c48838
Joshua Espinoza <josh6669@gmail.com>
2022-03-10 23:11:49 -0300

    Cuenta Regresiva Vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
-  <div id="app" class="container">
-    <div class="row">
+  <div id="app">
+    <div>
       <h1>Cuenta Regresiva</h1>
       <h2>{{ countdown }} segundos</h2>
       <div class="">
         <button class="btn btn-primary mx-2" @click="iniciar(3)">3 seg</button>
         <button class="btn btn-danger mx-2" @click="iniciar(10)">10 seg</button>
       </div>
+      <img alt="Vue logo" src="./assets/logo.png" />
     </div>
   </div>
 </template>
@@ -36,16 +37,9 @@ export default {
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-  ul {
-    li {
-      color: blue;
-    }
-  }
 }
 h2 {
   color: gray;
