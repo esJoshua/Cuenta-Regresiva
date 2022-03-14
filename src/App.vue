67e68db08ddfe801911e74d8f8bc88635d43001c
Joshua Espinoza <josh6669@gmail.com>
2022-03-13 22:45:14 -0300

    Cuenta Regresiva VUE

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,8 @@
       <h1>Cuenta Regresiva</h1>
       <h2>{{ countdown }} segundos</h2>
       <div class="">
-        <button class="btn btn-primary mx-2" @click="iniciar(3)">3 seg</button>
-        <button class="btn btn-danger mx-2" @click="iniciar(10)">10 seg</button>
+        <button class="btn btn-primary mx-2" @click="start(3)">3 seg</button>
+        <button class="btn btn-danger mx-2" @click="start(10)">10 seg</button>
       </div>
       <img alt="Vue logo" src="./assets/logo.png" />
     </div>
@@ -22,7 +22,7 @@ export default {
     };
   },
   methods: {
-    iniciar(time) {
+    start(time) {
       this.countdown = time;
       clearInterval(this.myInterval);
       this.myInterval = setInterval(() => {
